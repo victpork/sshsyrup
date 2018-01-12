189e7dd8e7d384925ed416a270748b7aca20862a
mk <224617+mkishere@users.noreply.github.com>
2018-01-13 01:14:05 +0800

    Modify internal FileInfo receiver
    Doesn't need pointer receiver, those functions doens't modify underlying value anyway

--- virtualfs/fileinfo.go
+++ virtualfs/fileinfo.go
@@ -10,20 +10,20 @@ type FileInfo struct {
 	n    *Node
 }
 
-func (fi *FileInfo) IsDir() bool { return fi.n.FileMode&os.ModeDir != 0 }
+func (fi FileInfo) IsDir() bool { return fi.n.FileMode&os.ModeDir != 0 }
 
-func (fi *FileInfo) ModTime() time.Time { return fi.n.modTime }
+func (fi FileInfo) ModTime() time.Time { return fi.n.modTime }
 
-func (fi *FileInfo) Mode() os.FileMode { return fi.n.FileMode }
+func (fi FileInfo) Mode() os.FileMode { return fi.n.FileMode }
 
-func (fi *FileInfo) Name() string {
+func (fi FileInfo) Name() string {
 	return fi.path
 }
 
-func (fi *FileInfo) Size() int64 {
+func (fi FileInfo) Size() int64 {
 	return fi.n.size
 }
 
-func (fi *FileInfo) Sys() interface{} {
+func (fi FileInfo) Sys() interface{} {
 	return nil
 }
