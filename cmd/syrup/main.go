25f5680496f746a66674f99b62965d01cb684e08
mkishere <224617+mkishere@users.noreply.github.com>
2018-02-13 21:06:01 +0800

    Cleanup commented code

--- cmd/syrup/main.go
+++ cmd/syrup/main.go
@@ -27,53 +27,11 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-// Config type is a map for storing config values
-/* type Config struct {
-	SvrAddr         string `json:"server.addr"`
-	SvrPort         int    `json:"server.port"`
-	SvrAllowRndUser bool   `json:"server.allowRandomUser"`
-	SvrVer          string `json:"server.ident"`
-	SvrMaxTries     int    `json:"server.maxTries"`
-	SvrMaxConn      int    `json:"server.maxConnections"`
-	SvrTimeout      int    `json:"server.timeout"`
-	SvrSpeed        int    `json:"server.speed"`
-	SvrDelay        int    `json:"server.processDelay"`
-	SvrHostname     string `json:"server.Hostname"`
-	SvrCmdList      string `json:"server.commandList"`
-	SessionLogFmt   string `json:"server.sessionLogFmt"`
-	VFSImgFile      string `json:"virtualfs.imageFile"`
-	VFSUIDMapFile   string `json:"virtualfs.uidMappingFile"`
-	VFSGIDMapFile   string `json:"virtualfs.gidMappingFile"`
-	VFSTempDir      string `json:"virtualfs.SavedFileDir"`
-	AcinemaAPIEndPt string `json:"asciinema.apiEndpoint"`
-	AcinemaAPIKey   string `json:"asciinema.apiKey"`
-} */
-
 const (
 	logTimeFormat string = "20060102"
 )
 
 var (
-	//config     = loadConfiguration("config.json")
-	/* defaultCfg = Config{
-		SvrAddr:         "0.0.0.0",
-		SvrPort:         2222,
-		SvrAllowRndUser: true,
-		SvrVer:          "SSH-2.0-OpenSSH_6.8p1",
-		SvrMaxTries:     3,
-		SvrMaxConn:      10,
-		SvrTimeout:      600,
-		SvrSpeed:        -1,
-		SvrDelay:        -1,
-		SvrHostname:     "spr1139",
-		SvrCmdList:      "commands.txt",
-		SessionLogFmt:   "asciinema",
-		VFSImgFile:      "filesystem.zip",
-		VFSUIDMapFile:   "passwd",
-		VFSGIDMapFile:   "group",
-		VFSTempDir:      "tempdir",
-		AcinemaAPIEndPt: "https://asciinema.org",
-	} */
 	vfs afero.Fs
 )
 
@@ -135,7 +93,6 @@ func init() {
 	}
 	// Initalize VFS
 	// ID Mapping
-	//uidMap, gidMap := loadIDMapping(config.VFSUIDMapFile), loadIDMapping(config.VFSGIDMapFile)
 
 	backupFS := afero.NewBasePathFs(afero.NewOsFs(), viper.GetString("virtualfs.savedFileDir"))
 	zipfs, err := virtualfs.NewVirtualFS(viper.GetString("virtualfs.imageFile"))
@@ -166,15 +123,6 @@ func main() {
 		bannerFile = []byte{}
 	}
 	sshConfig := &ssh.ServerConfig{
-		/* AuthLogCallback: func(c ssh.ConnMetadata, method string, err error) {
-			if method != "none" {
-				log.WithFields(log.Fields{
-					"user":       c.User(),
-					"srcIP":      c.RemoteAddr().String(),
-					"authMethod": method,
-				}).Infof("User trying to login with %v", method)
-			}
-		}, */
 
 		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
 			clientIP, port, _ := net.SplitHostPort(c.RemoteAddr().String())
