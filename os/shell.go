ff7bce379a6628bbd7eba25d7874628c5c22a9c6
mkishere <224617+mkishere@users.noreply.github.com>
2018-02-27 12:16:24 +0800

    Separate shell parse into 2 functions for future shlex implementation

--- os/shell.go
+++ os/shell.go
@@ -45,7 +45,6 @@ func (sh *Shell) HandleRequest(hook termlogger.LogHook) {
 			sh.termSignal <- 1
 		}
 	}()
-cmdLoop:
 	for {
 		cmd, err := sh.terminal.ReadLine()
 		if len(strings.TrimSpace(cmd)) > 0 {
@@ -54,42 +53,22 @@ cmdLoop:
 		if sh.DelayFunc != nil {
 			sh.DelayFunc()
 		}
-		cmd = strings.TrimSpace(cmd)
-		switch {
-		case err != nil:
+		if err != nil {
 			if err.Error() == "EOF" {
 				sh.log.WithError(err).Info("Client disconnected from server")
 				sh.termSignal <- 0
 				return
 			}
 			sh.log.WithError(err).Error("Error when reading terminal")
-			break cmdLoop
-		case strings.TrimSpace(cmd) == "":
-			//Do nothing
-		case cmd == "logout", cmd == "exit":
-			sh.log.Infof("User logged out")
-			sh.termSignal <- 0
-			return
-		case strings.HasPrefix(cmd, "cd"):
-			args := strings.Split(cmd, " ")
-			if len(args) > 1 {
-				err := sh.sys.Chdir(args[1])
-				if err != nil {
-					sh.terminal.Write([]byte(fmt.Sprintf("-bash: cd: %v: No such file or directory\n", args[1])))
-				}
+			break
+		}
+		if strings.Contains(cmd, ";") {
+			cmdList := strings.Split(cmd, ";")
+			for i := range cmdList {
+				sh.ExecCmd(cmdList[i], tLog)
 			}
-		case strings.HasPrefix(cmd, "export"):
-
-		default:
-			// TODO: parse script
-
-			args := strings.Split(cmd, " ")
-			n, err := sh.sys.exec(args[0], args[1:], tLog)
-			if err != nil {
-				sh.terminal.Write([]byte(fmt.Sprintf("%v: command not found\n", args[0])))
-			} else {
-				sh.sys.envVars["?"] = string(n)
-			}
+		} else {
+			sh.ExecCmd(cmd, tLog)
 		}
 	}
 }
@@ -98,4 +77,35 @@ func (sh *Shell) SetSize(width, height int) error {
 	sh.sys.width = width
 	sh.sys.height = height
 	return sh.terminal.SetSize(width, height)
+}
+
+func (sh *Shell) ExecCmd(cmd string, tLog termlogger.StdIOErr) {
+	cmd = strings.TrimSpace(cmd)
+	switch {
+
+	case strings.TrimSpace(cmd) == "":
+		//Do nothing
+	case cmd == "logout", cmd == "exit":
+		sh.log.Infof("User logged out")
+		sh.termSignal <- 0
+		return
+	case strings.HasPrefix(cmd, "cd"):
+		args := strings.Split(cmd, " ")
+		if len(args) > 1 {
+			err := sh.sys.Chdir(args[1])
+			if err != nil {
+				sh.terminal.Write([]byte(fmt.Sprintf("-bash: cd: %v: No such file or directory\n", args[1])))
+			}
+		}
+	case strings.HasPrefix(cmd, "export"):
+
+	default:
+		args := strings.Split(cmd, " ")
+		n, err := sh.sys.exec(args[0], args[1:], tLog)
+		if err != nil {
+			sh.terminal.Write([]byte(fmt.Sprintf("%v: command not found\n", args[0])))
+		} else {
+			sh.sys.envVars["?"] = string(n)
+		}
+	}
 }
